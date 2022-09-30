5705ab5399ad9c3b36375142feef28ad9d7bc0b2
sergij14 <sergi.jajanidze@gmail.com>
2022-09-30 19:34:43 +0400

    updated markup and styling

--- src/utils/getTemplate.ts
+++ src/utils/getTemplate.ts
@@ -10,15 +10,17 @@ export const getTemplate = (data: Config["data"], placeHolderText: string) => {
     .replace(/,/g, "");
 
   return `
-       <div class="select__input">
-           <div class="select__backdrop" data-type="backdrop"></div>
-           <span data-type="value">${placeHolderText}</span>
-           <i class="fa-solid fa-caret-up" data-type="arrow"></i>
+           <div class="select__input">
+              <div class="select__backdrop" data-type="backdrop"></div>
+              <span data-type="value">${placeHolderText}</span>
+              <i class="fa-solid fa-caret-up" data-type="arrow"></i>
            </div>
-           <div class="select__dropdown el-hi">
-           <ul class="select__list">
-               ${items}
-           </ul>
-       </div>
+           <div class="select__dropdown">
+              <div class="select__dropdown__inner">              
+                  <ul class="select__list">
+                      ${items}
+                  </ul>
+              </div>
+          </div>
    `;
 };
